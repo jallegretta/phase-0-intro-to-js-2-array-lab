b1c23c754905136907bd9b99fa6c62a2573d284e
Jared <jaralleg@amazon.com>
2021-03-18 21:18:34 -0400

    frustration.js

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+
+let cats=["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    return [...cats, "Broom"];
+}
+
+function prependCat(name){
+    return ["Arnold", ...cats]; 
+}
+
+function removeLastCat(){
+    let cats2 = cats.slice(0,2);
+    return cats2;
+}
+
+function removeFirstCat(){
+    let cats3 = cats.slice(1)
+    return cats3;
+}
